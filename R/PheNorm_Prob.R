13322f5793da4b47852609c3d6fac9cb87fc49da
Clara-Lea <claralea.bonzel@hotmail.fr>
2020-12-17 19:08:46 +0100

    Include \dontrun in the example

--- R/PheNorm_Prob.R
+++ R/PheNorm_Prob.R
@@ -14,11 +14,13 @@
 #' @param train.size size of training sample, default value 10 * nrow(dat)
 #' @return list containing probability and beta coefficient
 #' @examples
+#' \dontrun{
 #' set.seed(1234)
 #' fit.dat <- read.csv("https://raw.githubusercontent.com/celehs/PheNorm/master/data-raw/data.csv")
 #' fit.phenorm=PheNorm.Prob("ICD", "utl", fit.dat, nm.X = NULL,
-#' corrupt.rate=0.3, train.size=nrow(fit.dat))
+#'                           corrupt.rate=0.3, train.size=nrow(fit.dat));
 #' head(fit.phenorm$probs)
+#' }
 #' @export
 PheNorm.Prob <- function(nm.logS.ori, nm.utl, dat, nm.X = NULL, corrupt.rate = 0.3, train.size = 10 * nrow(dat)) {
   dat <- as.matrix(dat)
